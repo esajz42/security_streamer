5d278c96a8d73e186a749b880f3d67cd4ac1ffd7
josh <josh.zollweg@gmail.com>
2016-01-16 12:29:17 -0700

    Added video_feed end point to app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen(Camera()),
+            mimetype="multipart/x-mixed-replace; boundary=frame")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
