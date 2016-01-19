2b773d660efac3af828838b21a0a4b3c339824ce
josh <josh.zollweg@gmail.com>
2016-01-18 20:26:08 -0700

    Used global to share alerts setting between routes. Need to figure out the right way to do this...

--- app.py
+++ app.py
@@ -2,36 +2,33 @@
 from flask import Flask, render_template, Response, request
 from camera_pi import Camera
 
+alerts = False
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-def gen(camera, alerts):
+def gen(camera):
+    global alerts
     print "gen: " + str(alerts)
     while True:
         frame = camera.get_frame(alerts)
         yield (b"--frame\r\n"
                b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
-@app.route("/video_feed", methods=["POST", "GET"])
+@app.route("/video_feed")
 def video_feed():
-    alerts = False
-    if request.method == "POST":
-        if request.form["alerts"] == "Alerts":
-            alerts = True
-        else:
-            alerts = False
-    return Response(gen(Camera(), alerts),
+    return Response(gen(Camera()),
             mimetype="multipart/x-mixed-replace; boundary=frame")
 
-#@app.route("/control", methods=["POST"])
-#def control():
-#    if request.method == "POST":
-#        button = request.form["alerts"]
-#        session["alerts"] = button
-#        return button
+@app.route("/control", methods=["POST"])
+def control():
+    global alerts
+    # if request.method == "POST":
+    alerts = request.form["alerts"] == "Alerts"
+    return str(alerts)
 
 if __name__ == "__main__":
     app.run(host="192.168.0.16", port=8080, threaded=True, debug=True)
